9068f51b96905b49e0f5bdfad806f51e77a98601
malav2364 <malavp024@gmail.com>
2025-05-31 08:36:35 +0530

    Added Components and Shadcn ui library

--- frontend/excel_analytics_frontend/src/App.jsx
+++ frontend/excel_analytics_frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Button } from './components/ui/button'
+import { Input } from './components/ui/input'
 
 function App() {
   return (
-    <div className='flex items-center justify-center'>
+    <div className='flex flex-col  items-center justify-center'>
         <h1>Welcome to the Excel Analytics Platform</h1>
+        <h2>Signup</h2>
+        <Input type="username" placeholder="Username"/>
+        <Input type="email" placeholder="Email"/>
+        <Input type="password" placeholder="Password"/>
     </div>
   )
 }
